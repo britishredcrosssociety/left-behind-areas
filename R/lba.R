198fc522f828e75dfc6df02dc1570b418c771e9f
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-07-25 19:30:27 +0200

    Add map to app

--- R/lba.R
+++ R/lba.R
@@ -2,10 +2,28 @@ library(shiny)
 library(leaflet)
 
 lba <- function() {
-  ui <- fluidPage(
-    "Hello, world!"
+  ui <- bootstrapPage(
+
+    # - Add tags to render map fullscreen -
+    tags$head(
+      tags$link(href = "https://fonts.googleapis.com/css?family=Oswald", rel = "stylesheet"),
+      tags$style(type = "text/css", "html, body {width:100%;height:100%; font-family: Oswald, sans-serif;}"),
+      tags$style("h1 {color:#262626;}"),
+      tags$style("h5 {color:#5C747A;}")
+    ),
+
+    # - Call map module -
+    mapUI("map"),
+
+    # - Title & subheading -
+    absolutePanel(
+      top = 10, left = 20, style = "z-index:500; text-align: lwft;",
+      tags$h1("Explore Left Behind Areas"),
+      tags$h5("Left Behind Areas are shaded red. Local authority are dashed lines.")
+    )
   )
   server <- function(input, output, session) {
+    mapServer("map")
   }
   shinyApp(ui, server)
 }
