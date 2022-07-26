396e22134cdf3e924910f36ee2614d120d05da53
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-07-26 16:25:08 +0200

    Add source attribution

--- R/lba.R
+++ R/lba.R
@@ -9,7 +9,9 @@ lba <- function() {
       tags$link(href = "https://fonts.googleapis.com/css?family=Oswald", rel = "stylesheet"),
       tags$style(type = "text/css", "html, body {width:100%;height:100%; font-family: Oswald, sans-serif;}"),
       tags$style("h1 {color:#262626;}"),
-      tags$style("h5 {color:#5C747A;}")
+      tags$style("h4 {color:#5C747A;}"),
+      tags$style("p {color:#9CAAAE;}"),
+      tags$style("a {color:#9CAAAE;}")
     ),
 
     # - Call map module -
@@ -19,7 +21,13 @@ lba <- function() {
     absolutePanel(
       top = 10, left = 20, style = "z-index:500; text-align: lwft;",
       tags$h1("Explore Left Behind Areas"),
-      tags$h5("Left-behind areas are shaded red. Local authorities are dashed lines.")
+      tags$h4("Left-behind areas are shaded red. Local authorities are dashed lines."),
+      tags$p(
+        "Source:",
+        tags$a(href = "https://ocsi.uk/", target = "_blank", "OCSI"),
+        "&",
+        tags$a(href = "https://localtrust.org.uk/", target = "_blank", "Local Trust")
+      )
     )
   )
   server <- function(input, output, session) {
